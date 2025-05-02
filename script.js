820a6e71e66748c68007d1c278942bbe4610340f
majdweb <129631019+majdweb@users.noreply.github.com>
2025-05-02 18:54:12 +0300

    Update script.js

--- script.js
+++ script.js
@@ -6,12 +6,11 @@ var finalPrice=document.getElementById("finalPrice");
 elements.forEach(function(element){
   element.onclick=function() {
     totalPrice+=+(element.getAttribute("price"));
-    myCart.innerHTML+=`<h1>${element.getAttribute("productName")}</h1>`
+    myCart.innerHTML+=`<h1 class="paragraph">${element.getAttribute("productName")}</h1>`
   }
 })
 
 function handleFinalClick(){
   finalPrice.innerHTML=`${totalPrice}$`;
-  console.log(totalPrice);
  }
  finalButton.onclick=handleFinalClick;
